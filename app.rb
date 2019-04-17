630e5d61a342637451907add316778464cb6ec0c
Andrew Metcalf <andrew@stripe.com>
2019-04-16 21:29:41 -0700

    unlock

--- app.rb
+++ app.rb
@@ -1,7 +1,28 @@
 require 'sinatra'
+require 'excon'
 
 class App < Sinatra::Base
-  get "/" do
-    "Hello world!"
-  end
+    AUGUST_API_KEY = "79fd0eb6-381d-4adf-95a0-47721289d1d9"
+
+    def unlock
+        Excon.put(
+            "https://api-production.august.com/remoteoperate/#{ENV['LOCK_ID']}/lock",
+            headers: {
+                "x-august-api-key" => AUGUST_API_KEY,
+                "x-kease-api-key" => AUGUST_API_KEY,
+                "x-august-access-token" => ENV['AUGUST_ACCESS_TOKEN'],
+                "User-Agent" => "August/Luna-3.2.2",
+                "Accept-Version" => "0.0.1",
+            }
+        )
+    end
+
+    get "/" do
+        "Hello world! #{Time.now}"
+    end
+
+    get "/unlock" do
+        unlock
+        "done"
+    end
 end
