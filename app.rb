6463a953ae25f629e82911277c8245839cddb682
Andrew Metcalf <andrew@stripe.com>
2019-04-16 21:42:47 -0700

    push more debug logging

--- app.rb
+++ app.rb
@@ -3,19 +3,19 @@ require 'excon'
 
 class App < Sinatra::Base
     AUGUST_API_KEY = "79fd0eb6-381d-4adf-95a0-47721289d1d9"
+    LOCK_URL = "https://api-production.august.com/remoteoperate/#{ENV['LOCK_ID']}/lock"
 
     def unlock
-        resp = Excon.put(
-            "https://api-production.august.com/remoteoperate/#{ENV['LOCK_ID']}/lock",
-            headers: {
-                "x-august-api-key" => AUGUST_API_KEY,
-                "x-kease-api-key" => AUGUST_API_KEY,
-                "x-august-access-token" => ENV['AUGUST_ACCESS_TOKEN'],
-                "User-Agent" => "August/Luna-3.2.2",
-                "Accept-Version" => "0.0.1",
-                "Content-Type" => "application/json",
-            }
-        )
+        headers = {
+            "x-august-api-key" => AUGUST_API_KEY,
+            "x-kease-api-key" => AUGUST_API_KEY,
+            "x-august-access-token" => ENV['AUGUST_ACCESS_TOKEN'],
+            "User-Agent" => "August/Luna-3.2.2",
+            "Accept-Version" => "0.0.1",
+            "Content-Type" => "application/json",
+        }
+        $stderr.puts("unlock request: #{LOCK_URL} #{headers.inspect}")
+        resp = Excon.put(LOCK_URL, headers: headers)
 
         $stderr.puts("unlock response: #{resp.status} #{resp.body}")
 
